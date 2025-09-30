fd41024f3a3b80be6c14bd818d4e5cce956f5390
Kei07r <kei.skillsboost@gmail.com>
2025-09-30 07:00:32 +0530

    feat: add routing with react-router-dom and create NotFound page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ChatPage from "./pages/ChatPage";
+import NotFoundPage from "./pages/NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/chat",
+    element: <ChatPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={route} />
+  </StrictMode>
+);
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import landingPageImg from "../assets/homeImg.png";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div>
-      <div>
-        <img
-          className="w-[200px]"
-          src={landingPageImg}
-          alt="Landing Page Img"
-        />
-      </div>
-      <div>
-        <h1 className="text-h1 font-sans text-brand-secondary">
-          Carry Your Boats
-        </h1>
-        <p className="text-body font-sans text-brand-neutral max-w-md">
-          An AI-Powered Platform to bring you brack to your hard self.
-        </p>
-      </div>
-
-      <div className="bg-brand-primary text-white p-4">
-        Tailwind 3 brand color working 🎉
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center px-4 bg-gradient-to-br from-brand-primary via-brand-background to-brand-secondary">
+      <img
+        src={landingPageImg}
+        alt="Landing"
+        className="w-48 h-48 object-contain mb-gapMd"
+      />
+      <h1 className="text-brand-primary text-h1 font-sans mb-gapSm text-center">
+        Welcome to Carry Your Boats
+      </h1>
+      <p className="text-brand-neutral text-body font-sans mb-gapMd text-center max-w-xl">
+        <span className="block mt-2 text-brand-neutral opacity-80">
+          Unlock your potential and stay motivated every day.
+          <br />
+          "STAY HARD!"
+        </span>
+      </p>
+      <Link to="/chat">
+        <button className="bg-brand-secondary text-white px-btnX py-btnY rounded-lg text-body font-sans hover:bg-brand-primary transition">
+          Start Chatting
+        </button>
+      </Link>
     </div>
   );
 }
